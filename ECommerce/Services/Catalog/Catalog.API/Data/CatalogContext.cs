573beceb48e2497fc729b770c467e2e4040cd3c8
Marko Hrncic <marko.hrncic@kogitas.com>
2022-03-07 22:30:53 +0100

    Implemented interface and catalog context for connecting to Mongo DB database.

---
+++ ECommerce/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration["DatabaseSettings:ConnectionString"]);
+            var database = client.GetDatabase(configuration["DatabaseSettings:DatabaseName"]);
+
+            Products = database.GetCollection<Product>(configuration["DatabaseSettings:CollectionName"]);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
