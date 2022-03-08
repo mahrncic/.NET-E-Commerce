a43223043dac0535d8091fec61486455fc4b7c6a
Marko Hrncic <marko.hrncic@kogitas.com>
2022-03-08 18:17:11 +0100

    Added CatalogController class.

---
+++ ECommerce/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(IProductRepository repository, ILogger<CatalogController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+
+    }
+}
