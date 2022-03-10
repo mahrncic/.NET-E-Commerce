f84713a2eceaf9cd55d4c5eafe13fb25951bb984
Marko Hrncic <marko.hrncic@kogitas.com>
2022-03-10 19:50:29 +0100

    Implemented Get Basket method in Basket Repository.

---
+++ Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,37 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache;
+        }
+
+        public Task DeleteBasket(string userName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+
+            if (string.IsNullOrEmpty(basket))
+                return null;
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
