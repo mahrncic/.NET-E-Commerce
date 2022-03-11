89814d39881b9a51bca1df3c3b87693f6815964c
Marko Hrncic <marko.hrncic@kogitas.com>
2022-03-11 18:56:40 +0100

    Implemented Get Basket endpoint in the controller.

---
+++ ECommerce/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,33 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _repository = basketRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket = await _repository.GetBasket(userName);
+
+            if (basket == null)
+                return NotFound();
+
+            return Ok(basket);
+        }
+    }
+}
