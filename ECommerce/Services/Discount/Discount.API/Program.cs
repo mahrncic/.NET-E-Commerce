da250909207782534b96bc93b46e102292e0c5f7
Marko Hrncic <marko.hrncic@kogitas.com>
2022-03-15 19:52:32 +0100

    Implemented MigrateDatabase extension method.

--- ECommerce/Services/Discount/Discount.API/Program.cs
+++ ECommerce/Services/Discount/Discount.API/Program.cs
@@ -7,7 +7,10 @@ namespace Discount.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            host.MigrateDatabase<Program>();
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
