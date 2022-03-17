1240f800c576bd436dbcef564f9d8cf732ff7089
Marko Hrncic <marko.hrncic@kogitas.com>
2022-03-17 21:58:11 +0100

    Added new Discount Service based on the Discount Proto Service

---
+++ ECommerce/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,20 @@
+﻿using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+using Microsoft.Extensions.Logging;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService: DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly IDiscountRepository _discountRepository;
+        private readonly ILogger<DiscountService> _logger;
+
+        public DiscountService(IDiscountRepository discountRepository, ILogger<DiscountService> logger)
+        {
+            _discountRepository = discountRepository;
+            _logger = logger;
+        }
+
+
+    }
+}
