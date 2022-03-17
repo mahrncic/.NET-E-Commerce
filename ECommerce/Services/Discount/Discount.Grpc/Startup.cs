46fb05eb7b016cb9e0403479e984907e0a240719
Marko Hrncic <marko.hrncic@kogitas.com>
2022-03-17 22:05:44 +0100

    Configured Discount Grpc Service in the startup class.

--- ECommerce/Services/Discount/Discount.Grpc/Startup.cs
+++ ECommerce/Services/Discount/Discount.Grpc/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using Discount.Grpc.Repositories;
+using Discount.Grpc.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -26,6 +27,8 @@ namespace Discount.Grpc
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapGrpcService<DiscountService>();
+
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
