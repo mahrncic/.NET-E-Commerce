486e7209ef7ee36e91a63d05aff664515e111af7
Marko Hrncic <marko.hrncic@kogitas.com>
2022-03-18 22:26:04 +0100

    Implemented Update Basket method with the usage of gRPC discount logic in order to deduct the initial price.

--- ECommerce/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ ECommerce/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using Basket.API.Entities;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
@@ -11,10 +12,12 @@ namespace Basket.API.Controllers
     public class BasketController : ControllerBase
     {
         private readonly IBasketRepository _repository;
+        private readonly DiscountGrpcService _discountGrpcService;
 
-        public BasketController(IBasketRepository basketRepository)
+        public BasketController(IBasketRepository basketRepository, DiscountGrpcService discountGrpcService)
         {
             _repository = basketRepository;
+            _discountGrpcService = discountGrpcService;
         }
 
         [HttpGet("{userName}", Name = "GetBasket")]
@@ -30,6 +33,12 @@ namespace Basket.API.Controllers
         [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
         {
+            foreach(var item in basket.Items)
+            {
+                var itemDiscount = await _discountGrpcService.GetDiscount(item.ProductName);
+                item.Price -= itemDiscount.Amount;
+            }
+
             return Ok(await _repository.UpdateBasket(basket));
         }
 
