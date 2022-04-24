84bce71671d4a35930f6a34d2bba7fe1247b5886
Marko Hrncic <marko.hrncic@kogitas.com>
2022-04-24 14:44:31 +0200

    Implemented Ordering Service Get method with HttpClient in the Shopping.Aggregator API

---
+++ ECommerce/ApiGateways/Shopping.Aggregator/Services/OrderingService.cs
@@ -0,0 +1,24 @@
+﻿using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Services
+{
+    public class OrderingService : IOrderingService
+    {
+        private readonly HttpClient _client;
+
+        public OrderingService(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+        {
+            var response = await _client.GetAsync($"/api/v1/Order/{userName}");
+            return await response.ReadContentAs<List<OrderResponseModel>>();
+        }
+    }
+}
